949a2f0d491aeee5c90def302f9e15337ae76f3d
Tab0o33 <w.moinet@orange.fr>
2024-02-12 18:39:30 +0100

    refactor: mise en place du lazyloading pour le chargement du TodoModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: 'todos', loadChildren: () => import('./todo/todo.module').then(m => m.TodoModule) },
+    { path: '**', redirectTo: 'todos' }
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
